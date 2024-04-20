26a726fde0d40bfb0b4b64a7ad60ed763ddb8111
allenno chou <chouallenno@gmail.com>
2024-04-21 03:23:42 +0800

    1. add esp32 control stepper. 2. readme modified.

---
+++ ESP32_test/stepper/stepper.ino
@@ -0,0 +1,34 @@
+#define DIR_PIN 4
+#define STEP_PIN 5
+#define MS_PIN_1 32
+#define MS_PIN_2 33
+#define MS_PIN_3 25
+#define DELAY 1 //Each step will consume DELAY*2 milliseconds
+#define STEPS 3200 //one revolution if sixteenth precision
+
+void setup() {
+  pinMode(DIR_PIN, OUTPUT);
+  pinMode(STEP_PIN, OUTPUT);
+  pinMode(MS_PIN_1, OUTPUT);
+  pinMode(MS_PIN_2, OUTPUT);
+  pinMode(MS_PIN_3, OUTPUT); 
+  digitalWrite(MS_PIN_1, HIGH);
+  digitalWrite(MS_PIN_2, HIGH);
+  digitalWrite(MS_PIN_3, HIGH); //Let MS1, MS2, MS3 HIGH to sixteenth precise
+  digitalWrite(DIR_PIN, HIGH); //DIR HIGH: counter clock wise
+  digitalWrite(STEP_PIN, LOW);
+
+}
+
+void loop() {
+  
+  for (int i=0; i<STEPS; i++)
+  {
+    digitalWrite(STEP_PIN, HIGH);
+    delay(DELAY);
+    digitalWrite(STEP_PIN, LOW);
+    delay(DELAY);
+  }
+  delay(2000);
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # MakeCutter_ST
 Try to use STM32 to control all the devices in the Cutter.
+
+I try to use esp32 to control each of the device to check how to control the device by 3.3v MCU before directly utilize STM32. 
+The files are in the folder "ESP32_test"
+
+The ST MCU I use is STM32-NUCLEO-C031C6 which I borrowed from ST during 2024 MakeNTU contest. 
